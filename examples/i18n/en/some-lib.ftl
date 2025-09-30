d43e3493adea5d08479265919d59e6ea6d600810
FranzJvezNiceAxa <56778463+stayhydated@users.noreply.github.com>
2025-09-30 10:32:30 -0400

    add new i18n impl

---
+++ examples/i18n/en/some-lib.ftl
@@ -0,0 +1,43 @@
+## EnumCountry
+
+enum_country-China = China
+enum_country-France = France
+enum_country-UnitedStates = United States
+
+## PreferedLanguage
+
+prefered_language-Chinese = Chinese
+prefered_language-English = English
+prefered_language-French = French
+
+## User
+
+user = User
+
+## UserDescriptionFtl
+
+user_description_ftl = User Description Ftl
+user_description_ftl-age = Age
+user_description_ftl-balance = Balance
+user_description_ftl-birth_date = Birth Date
+user_description_ftl-country = Country
+user_description_ftl-email = Email
+user_description_ftl-enable_notifications = Enable Notifications
+user_description_ftl-preferred = Preferred
+user_description_ftl-skip_me = Skip Me
+user_description_ftl-subscribe_newsletter = Subscribe Newsletter
+user_description_ftl-username = Username
+
+## UserLabelFtl
+
+user_label_ftl = User Label Ftl
+user_label_ftl-age = Age
+user_label_ftl-balance = Balance
+user_label_ftl-birth_date = Birth Date
+user_label_ftl-country = Country
+user_label_ftl-email = Email
+user_label_ftl-enable_notifications = Enable Notifications
+user_label_ftl-preferred = Preferred
+user_label_ftl-skip_me = Skip Me
+user_label_ftl-subscribe_newsletter = Subscribe Newsletter
+user_label_ftl-username = Username
--- examples/some-lib/src/i18n.rs
+++ examples/some-lib/src/i18n.rs
@@ -1,68 +1,13 @@
-use i18n_embed::{
-    DefaultLocalizer, LanguageLoader as _, Localizer,
-    fluent::{FluentLanguageLoader, fluent_language_loader},
-    unic_langid::LanguageIdentifier,
-};
-use i18n_manager::I18nModule as I18nModuleTrait;
-use rust_embed::RustEmbed;
-use std::sync::LazyLock;
+use es_fluent_manager_singleton as i18n_manager;
 
-#[derive(RustEmbed)]
-#[folder = "../i18n/"]
-struct Localizations;
+es_fluent_manager_singleton::define_i18n_module!();
 
-pub static LANGUAGE_LOADER: LazyLock<FluentLanguageLoader> = LazyLock::new(|| {
-    let loader: FluentLanguageLoader = fluent_language_loader!();
-
-    loader
-        .load_fallback_language(&Localizations)
-        .expect("Error while loading fallback language");
-
-    #[cfg(test)]
-    loader.set_use_isolating(false);
-
-    loader
-});
-
-#[macro_export]
-macro_rules! fl {
-    ($message_id:literal) => {{
-        i18n_embed_fl::fl!($crate::i18n::LANGUAGE_LOADER, $message_id)
-    }};
-
-    ($message_id:literal, $($args:expr),*) => {{
-        i18n_embed_fl::fl!($crate::i18n::LANGUAGE_LOADER, $message_id, $($args), *)
-    }};
+pub fn init() {
+    i18n_manager::init();
 }
 
-#[must_use]
-fn localizer() -> Box<dyn Localizer> {
-    Box::from(DefaultLocalizer::new(&*LANGUAGE_LOADER, &Localizations))
-}
-
-struct I18nModule;
-
-impl I18nModuleTrait for I18nModule {
-    fn name(&self) -> &'static str {
-        env!("CARGO_CRATE_NAME")
-    }
-
-    fn init(&self, requested_languages: &[LanguageIdentifier]) -> anyhow::Result<()> {
-        localizer().select(requested_languages)?;
-        Ok(())
-    }
-
-    fn change_locale(&self, language: &str) -> anyhow::Result<()> {
-        let lang_id: LanguageIdentifier = language.parse()?;
-
-        let requested_languages = vec![lang_id];
-
-        localizer().select(&requested_languages)?;
-
-        Ok(())
-    }
-}
-
-inventory::submit! {
-    &I18nModule as &'static dyn I18nModuleTrait
+pub fn change_locale(language: &str) -> Result<(), unic_langid::LanguageIdentifierError> {
+    let lang_id: unic_langid::LanguageIdentifier = language.parse()?;
+    i18n_manager::select_language(&lang_id);
+    Ok(())
 }
