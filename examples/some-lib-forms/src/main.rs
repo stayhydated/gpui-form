f00b6e319ed97fff18511dfb7b57f7cce5a6fb29
FranzJvezNiceAxa <56778463+stayhydated@users.noreply.github.com>
2025-09-30 12:25:02 -0400

    use gpui_storybook's i18n context

--- examples/some-lib-forms/src/main.rs
+++ examples/some-lib-forms/src/main.rs
@@ -1,17 +1,15 @@
 use gpui::Application;
 use gpui_storybook::{Assets, Gallery};
 use some_lib_forms::forms;
-use some_lib_forms::i18n;
 
 fn main() {
     let app = Application::new().with_assets(Assets);
     let name_arg = std::env::args().nth(1);
 
     app.run(move |app_cx| {
-        i18n::init();
-        i18n::change_locale("en").unwrap();
         gpui_component::init(app_cx);
         gpui_storybook::init(app_cx);
+        gpui_storybook::change_locale("en").unwrap();
         forms::init(app_cx);
         app_cx.activate(true);
 
